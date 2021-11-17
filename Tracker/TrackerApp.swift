1c514a0ba377b9fe5f6a89d9c056d39e9203de24
Hamza Azhar <hamzaazhar.appiskey@gmail.com>
2021-11-17 17:17:24 +0500

    Initial Commit

---
+++ Tracker/TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerApp.swift
+//  Tracker
+//
+//  Created by Hamza_SCT on 11/17/21.
+//
+
+import SwiftUI
+
+@main
+struct TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
